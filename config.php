0fefa5a99363f2139215a5b5ef1c49df4583d807
Felix Moreira dos Santos <109150158+felixmoreira@users.noreply.github.com>
2023-01-26 22:35:38 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+$db = new DB(
+    "admin",
+    "123",
+    "chave1",
+    "0000000000000000",
+    "db/",
+    "dados/",
+    "arquivos/"
+);
+?>
